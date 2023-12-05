9947c4cbca769ae9edcf3d59442ea306dd3418ec
DaR00T <152205374+R00tUserr@users.noreply.github.com>
2023-12-05 12:20:05 -0500

    Update config.def.h

--- cherrywm/config.def.h
+++ cherrywm/config.def.h
@@ -73,13 +73,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *browcmd[]  = { "librewolf", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
         { MODKEY,                       XK_p,      spawn,          SHCMD ("rofi -show drun") },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_b,      spawn,          {.v = browcmd } },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
