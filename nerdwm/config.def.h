5ab20b012aa163fd72d8ef97dde34ea90f9c0537
Be1ac <119768937+Be1acProgrammer@users.noreply.github.com>
2023-12-08 22:21:08 +0000

    Update config.def.h

--- nerdwm/config.def.h
+++ nerdwm/config.def.h
@@ -11,7 +11,7 @@ static const int sloppyfocus               = 1;  /* focus follows mouse */
 static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will disable idle tracking even if it's surface isn't visible  */
 static const unsigned int borderpx         = 1;  /* border pixel of windows */
 static const float bordercolor[]           = COLOR(0x444444ff);
-static const float focuscolor[]            = COLOR(0x005577ff);
+static const float focuscolor[]            = COLOR(0x1A1A23ff);
 static const float urgentcolor[]           = COLOR(0xff0000ff);
 /* To conform the xdg-protocol, set the alpha to zero to restore the old behavior */
 static const float fullscreen_bg[]         = {0.1, 0.1, 0.1, 1.0}; /* You can also use glsl colors */
